e92d4073243ebec8920635c338b6769cd2959ed9
Brooks Davis <brooks@one-eyed-alien.net>
2022-11-02 22:39:39 +0000

    CHERI: Allow ioctl structs to contain capabilities
    Use switch from uint64_t to uint64ptr_t in ioctl structures.  For
    targets other than pure-capability CHERI this is a no-op.  Alter
    consumers to cast through uintptr_t and use uintptr_t storage to handle
    these values.

--- include/os/freebsd/spl/sys/sysmacros.h
+++ include/os/freebsd/spl/sys/sysmacros.h
@@ -365,6 +365,15 @@ highbit64(uint64_t i)
 #endif
 }
 
+#if !defined(__CHERI__) && !defined(__CHERI_HYBRID__)
+#ifndef copyinptr
+#define	copyinptr	copyin
+#endif
+#ifndef copyoutptr
+#define	copyoutptr	copyout
+#endif
+#endif
+
 #ifdef	__cplusplus
 }
 #endif
